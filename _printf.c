200ca42bf6a5523e6c5467951c77b909d2276cd9
simararustam <simararustam@gmail.com>
2024-03-25 23:12:56 +0000

    new printf file

---
+++ _printf.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include "main.h"
+/**
+ *
+ */
+int _printf(const char *format, ...)
+{
+	va_list argList;
+	int numChar = 0, i, j;
+
+	va_start(argList, format);
+	
+	for (i = 0; format[i] != 0; i++)
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+		}
+		else if (format[i] == '%' && format[i + 1] == 'c')
+		{
+			_putchar(va_arg(argList, int));
+			i++;
+		}
+		else if (format[i] == '%' && format[i + 1] == 's')
+		{
+			j = 0;
+			char *str = va_arg(argList, char *);
+			
+			while (str[j] != 0)
+			{
+				_putchar(str[j]);
+				j++;
+			}
+			i++;
+		}
+		else if (format[i] == '%' && format[i + 1] == '%')
+		{
+			_putchar('%');
+		}
+		numChar++;
+	}
+	_putchar('\n');
+	va_end(argList);
+	return (numChar);
+}
--- test/main.c
+++ test/main.c
@@ -8,5 +8,7 @@ int main(void)
 	_printf("simar%c", c);
 	_printf("%s", "string");
 	_printf("%%");
+	_printf("%c%s%%", 'c', "str");
+	printf("%s%c%c%c%s%%%s%c", "Loading ", '.', '.', '.', " 99", " Please wait", '\n');
 	return (0);
 }
