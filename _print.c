f97243766302801e8d63f3e5c9e58f6c0187de88
root <nihad.nemetli@gmail.com>
2024-03-25 15:35:56 +0000

    _print.c file error fixed

--- _print.c
+++ _print.c
@@ -7,31 +7,28 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list argumentList; //List of Arguments
-    int length = 0; //Number of characters
-    int i; //i is used for loop. Index of format string
-    //char *s;
-    va_start(argumentList, format); //Initialize argumentList
+    va_list argumentList; /*List of Arguments*/
+    int length = 0; /*Number of characters*/
+    int i; /*i is used for loop. Index of format string*/
+
+    va_start(argumentList, format); /*Initialize argumentList*/
     
-    //Loops and other thing need to be writed in between va_start and va_end
+    /*Loops and other thing need to be writed in between va_start and va_end*/
     for (i = 0; format[i] != '\0'; i++)
     {
         if (format[i] != '%')
         {
-            //If there is no type specifier just print what we have
+            /*If there is no type specifier just print what we have*/
             putchar(format[i]);
         }
         
-        //If there is a % sign and after % sign there is c print character
+        /*If there is a % sign and after % sign there is c print character*/
         if (format[i] == '%' && format[i + 1] == 'c')
         {
             putchar(va_arg(argumentList, int));
             i++;
         }
-        else if (format[i] == '%' && format[i + 1] == 's')
-        {
-            putchar(va_arg(argumentList, char *));
-        }
+
         
         length++;
         
