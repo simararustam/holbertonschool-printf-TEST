6c974ec754a3e9165e429050f1c06008980124ae
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-03-26 01:22:58 +0400

    Update _print.c

--- _print.c
+++ _print.c
@@ -36,9 +36,10 @@ int _printf(const char *format, ...)
 		else if (format[i] == '%' && format[i + 1] == 's')
 		{
 			char *str;
+			int stringLength = 0;
 			if (str == NULL)
 				str = "(null)";
-			int stringLength = 0;
+			
 			stringLength = _puts(va_arg(argumentList, char *));
 			i++;
 			length += stringLength - 1;
