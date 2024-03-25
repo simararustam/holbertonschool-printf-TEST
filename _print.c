b924222500ec7398f03eb147f54b0e87a95bfb4d
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-03-26 03:50:16 +0400

    Update _print.c

--- _print.c
+++ _print.c
@@ -52,6 +52,7 @@ int _printf(const char *format, ...)
 		else if (format[i] == '%'  && format[i + 1] != '%')
 		{
 			_putchar(format[i]);
+			i++;
 		}
 
 		else if (format[i] == '%'  && format[i + 1] == '\0')
