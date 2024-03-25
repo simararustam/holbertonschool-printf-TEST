e95c9dcceccf8a112007f987893e23c87cca7e61
simararustam <simararustam@gmail.com>
2024-03-25 23:49:20 +0000

    %

--- _printf.c
+++ _printf.c
@@ -3,7 +3,9 @@
 #include <stdarg.h>
 #include "main.h"
 /**
- *
+ *_printf - printf function
+ *@format: is a character string
+ *Return: number of character printed
  */
 int _printf(const char *format, ...)
 {
@@ -11,7 +13,7 @@ int _printf(const char *format, ...)
 	int numChar = 0, i, j;
 
 	va_start(argList, format);
-	
+
 	for (i = 0; format[i] != 0; i++)
 	{
 		if (format[i] != '%')
@@ -27,7 +29,7 @@ int _printf(const char *format, ...)
 		{
 			j = 0;
 			char *str = va_arg(argList, char *);
-			
+
 			while (str[j] != 0)
 			{
 				_putchar(str[j]);
@@ -41,6 +43,7 @@ int _printf(const char *format, ...)
 		}
 		else if (format[i] == '%' && format[i + 1] == 0)
 		{
+			_putchar("");
 			return (1);
 		}
 		numChar++;
