d107f3b19d2f732f4e62c39fd11fbceaf9286aa2
root <nihad.nemetli@gmail.com>
2024-03-25 15:32:58 +0000

    main error fixed

--- main.h
+++ main.h
@@ -1,6 +1,7 @@
 #ifndef _MAIN_H
 #define _MAIN_H
-//Function prortypes writed here
+
+/*Function prortypes writed here*/
 int _printf(const char *format, ...);
-int _putchar(char c);
+void _putchar(char c);
 #endif
