a464da969db361814fc641360878a118689963ac
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-03-26 10:51:47 +0400

    Update _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -9,9 +9,9 @@
 int _putchar(char c)
 {
 	/**
- 	* In write function 1st parameter used to
+	* In write function 1st parameter used to
 	* print or input. 1-print 0-input.
-  	* 2nd paramter is used for printing character.
+	* 2nd paramter is used for printing character.
 	* In here our character is c. 3rd parameter
 	* used for how many character we printi. In there we print 1
 	* character. Because putchar print only 1 character
