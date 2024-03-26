03610aa9272718662d72ec73f7f018d3e6d63e9a
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-03-26 18:17:07 +0400

    Update _print.c

--- _print.c
+++ _print.c
@@ -34,7 +34,8 @@ int _printf(const char *format, ...)
 		else if (format[i] == '%' && (format[i + 1] == 'd' || format[i + 1] == 'i'))
 		{
 			char number;
-			number += _putchar(_itoa(va_arg(argumentList, int)));
+			number += _itoa(va_arg(argumentList, int));
+			_putchar(number);
 			i++;
 		}		
 
