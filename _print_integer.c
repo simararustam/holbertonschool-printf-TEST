27c0da36a0750f0336f35a74d370728ac5ac875d
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-03-26 16:36:42 +0400

    Update _print_integer.c

--- _print_integer.c
+++ _print_integer.c
@@ -17,7 +17,13 @@ int _printf(const char *format, ...)
 	va_start(argumentList, format); /*Initialize argumentiList*/
 	for (i = 0; format[i] != '\0'; i++)
 	{
-		
+		if (format[i] != '%')
+			_putchar(format[i]);
+		if (format[i] == '%' && format[i + 1] == 'd')
+		{
+			_putchar(va_arg(argumentList, int));
+			i++;
+		}
 	}
 	va_end(argumentList);
 	return (length);
