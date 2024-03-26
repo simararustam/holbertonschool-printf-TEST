f5f94c6db7ed479d7022fa960b2aabf6d162d8d6
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-03-26 16:36:17 +0400

    Update _print_integer.c

--- _print_integer.c
+++ _print_integer.c
@@ -1,1 +1,24 @@
 /*test file*/
+#include "main.h"
+#include <stdarg.h>
+#include <stddef.h>
+#include <unistd.h>
+
+/**
+ * _printf - print something
+ * @format: type specifier
+ * Return: the number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list argumentList; /*List of Arguments*/
+	int length = 0, i; /*Number of characters*/
+
+	va_start(argumentList, format); /*Initialize argumentiList*/
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		
+	}
+	va_end(argumentList);
+	return (length);
+}
