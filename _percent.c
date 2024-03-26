de488576d4bbd3f440df3ecd4839bc0cce9e4652
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-03-26 10:36:22 +0400

    Update _percent.c

--- _percent.c
+++ _percent.c
@@ -1,18 +1,18 @@
 #include "main.h"
-    /*Part of printting '%'*/
-    else if (format[i] == '%' && format[i + 1] == '%')
-		{
-			/*write(1, "%", 1);*/
-			_putchar('%');
-			i++;
-		}
+    	/*Part of printting '%'*/
+	if (format[i] == '%' && format[i + 1] == '%')
+	{
+		/*write(1, "%", 1);*/
+		_putchar('%');
+		i++;
+	}
 
-		else if (format[0] == '%'  && format[1] == '\0')
-		{
-			return (-1);
-		}
+	else if (format[0] == '%'  && format[1] == '\0')
+	{
+		return (-1);
+	}
 
-		else if (format[i] == '%'  && format[i + 1] != '%')
-		{
-			_putchar(format[i]);
-		}
+	else if (format[i] == '%'  && format[i + 1] != '%')
+	{
+		_putchar(format[i]);
+	}
