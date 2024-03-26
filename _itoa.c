cf706aa450794b0340e157e4ea720a919a72746a
Simara <115341811+simararustam@users.noreply.github.com>
2024-03-26 16:28:18 +0400

    Create _itoa.c

---
+++ _itoa.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ *itoa - change integer to string
+ *@value: given integer
+ *Return: size of returned integer
+ */
+int _itoa(int value)
+{
+	char *str;
+	int size = 0, i = 0, j, n;
+
+	if (value < 0)
+	{
+		_putchar('-');
+		n = -value;
+		size++;
+	}
+	else
+		n = value;
+	
+	do {
+		n /= 10;
+		size++;
+	} while (n > 0);
+	
+	str = malloc(size);
+
+	i = 0;
+	do {
+		str[i++] = '0' + (n % 10);
+		n /= 10;
+	} while (n > 0);
+	str[i] = '\0';
+
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(str[j]);
+	}
+	free(str);
+	return (size);
+}
