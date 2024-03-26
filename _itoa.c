c150a8c3b8e4aec6fe4f7a1e1b18ea2edf13ddfe
Simara <115341811+simararustam@users.noreply.github.com>
2024-03-27 01:12:10 +0400

    Update _itoa.c

--- _itoa.c
+++ _itoa.c
@@ -4,57 +4,39 @@
 #include <limits.h>
 /**
  *_itoa - change integer to string
- *@value: given integer
+ *@a: given integer
  *Return: size of returned integer
  */
-int _itoa(int value)
+int _itoa(int a)
 {
-	char *str;
-	int size = 0, i = 0, j, n, temp, is_negative = 0, k = 0;
+	char *buffer;
+	int temp = a, size = 0, is_negative = 0, k = 0;
 
-   	 if (value < 0) 
-   	 {
-		 _putchar('-');
-		 n = -value;
-		 size++;
-		 is_negative = 1;
-    	} 
-    	else if (value == 0) 
-  		size++;
-    	else 
-        	n = value;
-
-    	temp = n;
-    	do {
-        	n /= 10;
-        	size++;
-    	} while (n);
-    	str = malloc(size);
-	if (str == NULL)
-		exit(77);
+	if (a < 0)
+		is_negative = 1;
+	do {
+		temp /= 10;
+		size++;
+	} while (temp);
 	if (is_negative)
 	{
-		if (value == INT_MIN)
-			value += 1, k = 1;
+		if (a == INT_MIN)
+			a += 1, k = 1;
 		size++;
-		value *= -1;
-	}	
-    	/* Reset n to its original value for conversion to string */
-    	n = temp;
-    	i = 0;
-    	do {
-        	str[i++] = (n % 10) + '0';
-        	n /= 10;
-    	} while (n);
-
-    	for (j = size - 1; j >= 0; j--) {
-        	_putchar(str[j]);
-    	}
-
+		a *= -1;
+	}
+	buffer = malloc(size);
+	if (buffer == NULL)
+		exit(77);
+	temp = 0;
+	for (temp = size - 1; temp >= 0; temp--)
+	{
+		buffer[temp] = a % 10 + '0';
+		a /= 10;
+	}
 	if (k)
-		str[size - 1] += 1;
+		buffer[size - 1] += 1;
 	if (is_negative)
-		str[0] = '-';
-    	free(str);
-    	return (size);
+		buffer[0] = '-';
+	return (size);
 }
