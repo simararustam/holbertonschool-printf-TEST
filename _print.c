b0de610b2d2a2af217d854f2812af99892247bf5
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-03-26 10:28:18 +0400

    Update _print.c

--- _print.c
+++ _print.c
@@ -32,7 +32,7 @@ int _printf(const char *format, ...)
 		/**
 		* we use int below. Because characters are promoted to int
 		* when passed as variable arguments.
-	 	*/
+		*/
 			_putchar(va_arg(argumentList, int));
 			i++;
 		}
