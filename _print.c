b1c4c266d21c376000cfb037414ea7bd01ce2186
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-03-26 18:05:40 +0400

    Update _print.c

--- _print.c
+++ _print.c
@@ -34,8 +34,8 @@ int _printf(const char *format, ...)
 		else if (format[i] == '%' && (format[i + 1] == 'd' || format[i + 1] == 'i'))
 		{
 			length += _itoa(va_arg(argumentList, int));
+			i++;
 			_putchar(length);
-			/*i++;*/
 		}		
 
 		if (format[i] == '%' && format[i + 1] == '%')
