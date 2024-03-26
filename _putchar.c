61d7682f8e6c2d48a29e321df613880d529ed800
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-03-26 10:50:31 +0400

    Update _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -8,12 +8,13 @@
  */
 int _putchar(char c)
 {
-	/*In write function 1st parameter used to 
-	 * print or input. 1-print 0-input.
-	 * 2nd paramter is used for printing character.
-	 * In here our character is c. 3rd parameter 
-	 * used for how many character we printi. In there we print 1
-	 * character. Because putchar print only 1 character 
-	 */
+	/**
+ 	* In write function 1st parameter used to
+	* print or input. 1-print 0-input.
+  	* 2nd paramter is used for printing character.
+	* In here our character is c. 3rd parameter
+	* used for how many character we printi. In there we print 1
+	* character. Because putchar print only 1 character
+	*/
 	return (write(1, &c, 1));
 }
