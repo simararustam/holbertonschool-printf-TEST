93e5ec7ff488d2857cac82aa7d9f4254eb91ead2
Simara <115341811+simararustam@users.noreply.github.com>
2024-03-27 01:10:28 +0400

    Update _itoa.c

--- _itoa.c
+++ _itoa.c
@@ -9,13 +9,14 @@
 int _itoa(int value)
 {
 	char *str;
-	int size = 0, i = 0, j, n, temp;
+	int size = 0, i = 0, j, n, temp, is_negative = 0, k = 0;
 
    	 if (value < 0) 
    	 {
 		 _putchar('-');
 		 n = -value;
 		 size++;
+		 is_negative = 1;
     	} 
     	else if (value == 0) 
   		size++;
@@ -27,14 +28,21 @@ int _itoa(int value)
         	n /= 10;
         	size++;
     	} while (n);
-
     	str = malloc(size);
-	
+	if (str == NULL)
+		exit(77);
+	if (is_negative)
+	{
+		if (value == INT_MIN)
+			value += 1, k = 1;
+		size++;
+		value *= -1;
+	}	
     	/* Reset n to its original value for conversion to string */
     	n = temp;
     	i = 0;
     	do {
-        	str[i++] = '0' + (n % 10);
+        	str[i++] = (n % 10) + '0';
         	n /= 10;
     	} while (n);
 
@@ -42,6 +50,10 @@ int _itoa(int value)
         	_putchar(str[j]);
     	}
 
+	if (k)
+		str[size - 1] += 1;
+	if (is_negative)
+		str[0] = '-';
     	free(str);
     	return (size);
 }
