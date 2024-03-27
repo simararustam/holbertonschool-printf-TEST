23b643b063e568128a04801afe5e7432a4c35d4e
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-03-27 14:14:45 +0400

    Update main.h

--- main.h
+++ main.h
@@ -4,7 +4,6 @@
 /*Function prortypes writed here*/
 int _itoa(int value);
 int _printf(const char *format, ...);
-int print_decimal(unsigned int r, int s)
 int _putchar(char c);
 int _puts(char *str);
 #endif
