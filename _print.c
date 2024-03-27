f17f8db4f891bdf0c8ef74b68abe4aca6e10e7a7
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-03-27 20:59:00 +0400

    Update _print.c

--- _print.c
+++ _print.c
@@ -33,8 +33,18 @@ int _printf(const char *format, ...)
 		}
 		else if (format[i] == '%' && (format[i + 1] == 'd' || format[i + 1] == 'i'))
 		{
-			length += _itoa(va_arg(argumentList, int)) - 1;
-			i++;
+			/*
+			* length += _itoa(va_arg(argumentList, int)) - 1;
+			* i++;
+			*/
+			char *p_buff;
+			int size;
+
+			p_buff = itoa(va_arg(argumentList, int), 10);
+
+			size = _putchar((p_buff != NULL) ? p_buff : "NULL");
+
+			return (size);
 		}
 		if (format[i] == '%' && format[i + 1] == '%')
 		{
