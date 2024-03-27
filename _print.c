e934c39edb645d8fb6a70980f94a238c8fde78a7
Simara <115341811+simararustam@users.noreply.github.com>
2024-03-27 18:35:32 +0400

    Update _print.c

--- _print.c
+++ _print.c
@@ -37,7 +37,7 @@ int _printf(const char *format, ...)
 			
 			integerLength += _itoa(va_arg(argumentList, int));
 			i++;
-			length += integerLength - 1;
+			length += integerLength - 2;
 		}
 		if (format[i] == '%' && format[i + 1] == '%')
 		{
