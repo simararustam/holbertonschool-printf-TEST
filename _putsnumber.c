5fd98f33f6cd4b43c78b800e139ecbfa840161ff
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-03-28 13:14:07 +0400

    Update _putsnumber.c

--- _putsnumber.c
+++ _putsnumber.c
@@ -1,1 +1,27 @@
+#include "main.h"
+#include <stddef.h>
 
+/**
+ * _puts - print string
+ * @str: string
+ * Return: void
+ */
+int _puts(char *str)
+{
+	int length;
+
+	if (str == NULL)
+	{
+		str = "(null)";
+	}
+
+	if (str)
+	{
+		for (length = 0; str[length] != '\0'; length++)
+		{
+			/*_putchar(str[length]);*/
+		}
+	}
+
+	return (length);
+}
