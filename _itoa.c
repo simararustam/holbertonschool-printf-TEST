263f6c166491b3aff86d519f72a6b47f33a90f8b
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-03-28 13:13:16 +0400

    Update _itoa.c

--- _itoa.c
+++ _itoa.c
@@ -10,47 +10,48 @@
  */
 int _itoa(int num)
 {
-    	int i = 0, j, k = 0,temp, len = 0;
-    	int isNegative = 0;
-    	char *str;
+	int i = 0, j, k = 0, temp, len = 0;
+	int isNegative = 0;
+	char *str;
 
-    	if (num < 0) {
-        	isNegative = 1;
-        	num = -num;
-        	len++;
-    	}
+	if (num < 0)
+	{
+		isNegative = 1;
+		num = -num;
+		len++;
+	}
 
 	temp = num;
 	do {
 		num /= 10;
 		len++;
 	} while (num > 0);
-	
+
 	if (isNegative)
 	{
-	    if (num == INT_MIN)
-	        num += 1, k = 1;
+		if (num == INT_MIN)
+			num += 1, k = 1;
 	}
-	
+
 	str = malloc(len);
 	if (str == NULL)
 		exit(77);
-	
+
 	num = temp;
-    	do {
-        	str[i++] = num % 10 + '0';
-       		num /= 10;
-    	} while (num > 0);
-    
-    	if (isNegative)
-        	str[i++] = '-';
-    	str[i] = '\0';
+	do {
+		str[i++] = num % 10 + '0';
+		num /= 10;
+	} while (num > 0);
+
+	if (isNegative)
+		str[i++] = '-';
+	str[i] = '\0';
 
 	for (j = i - 1; j >= 0; j--)
 		_putchar(str[j]);
 
-	if(k)
-	    str[i - 1] += 1;
+	if (k)
+		str[i - 1] += 1;
 
 	len = 0;
 	len = _puts(str);
