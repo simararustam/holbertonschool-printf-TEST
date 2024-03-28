28507cef768cec80572afb05b4f510bb60b17891
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-03-28 13:19:27 +0400

    Update _itoa.c

--- _itoa.c
+++ _itoa.c
@@ -13,30 +13,26 @@ int _itoa(int num)
 	int i = 0, j, k = 0, temp, len = 0;
 	int isNegative = 0;
 	char *str;
-
+	
 	if (num < 0)
 	{
 		isNegative = 1;
 		num = -num;
 		len++;
 	}
-
 	temp = num;
 	do {
 		num /= 10;
 		len++;
 	} while (num > 0);
-
 	if (isNegative)
 	{
 		if (num == INT_MIN)
 			num += 1, k = 1;
 	}
-
 	str = malloc(len);
 	if (str == NULL)
 		exit(77);
-
 	num = temp;
 	do {
 		str[i++] = num % 10 + '0';
@@ -46,7 +42,6 @@ int _itoa(int num)
 	if (isNegative)
 		str[i++] = '-';
 	str[i] = '\0';
-
 	for (j = i - 1; j >= 0; j--)
 		_putchar(str[j]);
 	if (k)
